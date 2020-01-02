775e98d3abfc60800d3eb044c7e7fa9f43762989
kuantzuhsiang <kuan.tzuhsiang@u.nus.edu>
2020-01-03 01:11:30 +0800

    removed entire listener

--- assets-launch/js/main.js
+++ assets-launch/js/main.js
@@ -102,37 +102,37 @@
 		})();
 
 	// Signup Form.
-		(function() {
+		// (function() {
 
-			// Vars.
-				var $form = document.querySelectorAll('#signup-form')[0],
-					$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
-					$message;
+		// 	// Vars.
+		// 		var $form = document.querySelectorAll('#signup-form')[0],
+		// 			$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
+		// 			$message;
 
-			// Bail if addEventListener isn't supported.
-				if (!('addEventListener' in $form))
-					return;
+		// 	// Bail if addEventListener isn't supported.
+		// 		if (!('addEventListener' in $form))
+		// 			return;
 
-			// Message.
-				$message = document.createElement('span');
-					$message.classList.add('message');
-					$form.appendChild($message);
+		// 	// Message.
+		// 		$message = document.createElement('span');
+		// 			$message.classList.add('message');
+		// 			$form.appendChild($message);
 
-				$message._show = function(type, text) {
+		// 		$message._show = function(type, text) {
 
-					$message.innerHTML = text;
-					$message.classList.add(type);
-					$message.classList.add('visible');
+		// 			$message.innerHTML = text;
+		// 			$message.classList.add(type);
+		// 			$message.classList.add('visible');
 
-					window.setTimeout(function() {
-						$message._hide();
-					}, 3000);
+		// 			window.setTimeout(function() {
+		// 				$message._hide();
+		// 			}, 3000);
 
-				};
+		// 		};
 
-				$message._hide = function() {
-					$message.classList.remove('visible');
-				};
+		// 		$message._hide = function() {
+		// 			$message.classList.remove('visible');
+		// 		};
 
 			// Events.
 			// Note: If you're *not* using AJAX, get rid of this event listener.
