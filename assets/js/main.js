ff0648915b9edf29ec6b75c115863c7ba698956e
kuantzuhsiang <kuan.tzuhsiang@u.nus.edu>
2020-01-09 00:55:04 +0800

    add debug statement

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,6 +177,7 @@
 	// Get the image and insert it inside the modal - use its "alt" text as a caption
 	// var img = document.getElementById("myImg");
 	var modalImg = document.getElementById("expandedImage");
+	console.log(modalImg);
 	// var captionText = document.getElementById("caption");
 	modalImg.onclick = function(){
 	  modal.style.display = "block";
